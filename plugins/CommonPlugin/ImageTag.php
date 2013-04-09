b064908b78fbe13c94d4f1a9da425d5d0a6ae41e
Duncan Cameron <phplist@dcameron.me.uk>
2013-04-09 11:55:45 +0100

    Encapsulate how an image file is served

---
+++ plugins/CommonPlugin/ImageTag.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * CommonPlugin for phplist
+ * 
+ * This file is a part of CommonPlugin.
+ *
+ * @category  phplist
+ * @package   CommonPlugin
+ * @author    Duncan Cameron
+ * @copyright 2011-2012 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ * @version   SVN: $Id: PageURL.php 804 2012-07-13 14:18:48Z Duncan $
+ * @link      http://forums.phplist.com/viewtopic.php?f=7&t=35427
+ */
+
+/**
+ * Class to create a URL for an image served through the plugin
+ * Encapsulates how the image is served
+ */
+class CommonPlugin_ImageTag
+{
+	/*
+	 *	Public methods
+	 */
+	public function __construct($image, $title)
+	{
+        $this->image = $image;
+        $this->title = $title;
+        $this->alt = $title;
+	}
+
+	public function __toString()
+	{
+        $imageUrl = new CommonPlugin_PageURL(null, array('action' => 'image', 'image' => $this->image));
+		return CHtml::tag('img', array('src'=> $imageUrl, 'alt' => $this->alt, 'title' => $this->title));
+	}
+}
