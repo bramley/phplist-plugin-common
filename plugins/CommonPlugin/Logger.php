a690c51fe4270674c15bed9b46ec0340cc398bf3
Duncan Cameron <phplist@dcameron.me.uk>
2015-01-25 13:01:59 +0000

    Use null logger when threshold is not provided

--- plugins/CommonPlugin/Logger.php
+++ plugins/CommonPlugin/Logger.php
@@ -7,7 +7,7 @@
  * @category  phplist
  * @package   CommonPlugin
  * @author    Duncan Cameron
- * @copyright 2011-2012 Duncan Cameron
+ * @copyright 2011-2015 Duncan Cameron
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
  */
 
@@ -17,22 +17,13 @@
  * 
  */ 
 use Psr\Log\LogLevel;
+use Psr\Log\NullLogger;
 
 class CommonPlugin_Logger extends Katzgrau\KLogger\Logger
 {
     private static $instance;
     private $threshold;
     private $classes;
-    private $logLevels = array(
-        LogLevel::EMERGENCY => 0,
-        LogLevel::ALERT     => 1,
-        LogLevel::CRITICAL  => 2,
-        LogLevel::ERROR     => 3,
-        LogLevel::WARNING   => 4,
-        LogLevel::NOTICE    => 5,
-        LogLevel::INFO      => 6,
-        LogLevel::DEBUG     => 7,
-    );
 
     /*
      *    Public methods
@@ -45,7 +36,7 @@ class CommonPlugin_Logger extends Katzgrau\KLogger\Logger
      * @param string $severity     One of the pre-defined PSR severity constants
      * @return CommonPlugin_Logger
      */
-    static public function instance($logDirectory = false, $severity = false)
+    static public function instance()
     {
         global $log_options;
         global $tmpdir;
@@ -53,30 +44,27 @@ class CommonPlugin_Logger extends Katzgrau\KLogger\Logger
         if (isset(self::$instance))
             return self::$instance;
 
-        if ($logDirectory) {
-            $dir = $logDirectory;
-        } elseif (isset($log_options['dir'])) {
-            $dir = $log_options['dir'];
-        } elseif (isset($tmpdir)) {
-            $dir = $tmpdir;
+        if (isset($log_options['threshold']) && defined('Psr\Log\LogLevel::' . $log_options['threshold'])) {
+            $threshold = constant('Psr\Log\LogLevel::' . $log_options['threshold']);
+
+            if (isset($log_options['dir'])) {
+                $dir = $log_options['dir'];
+            } elseif (isset($tmpdir)) {
+                $dir = $tmpdir;
+            } else {
+                $dir = '/var/tmp';
+            }
+
+            if (isset($_GET['pi'])) {
+                $pi = preg_replace('/\W/', '', $_GET['pi']);
+                $dir .= '/' . $pi;
+            }
+            $logger = new self($dir, $threshold);
+            $logger->setDateFormat('D d M Y H:i:s');
         } else {
-            $dir = '/var/tmp';
+            $logger = new NullLogger();
         }
 
-        if ($severity) {
-            $threshold = $severity;
-        } elseif (isset($log_options['level']) && defined($log_options['level'])) {
-            $threshold = constant($log_options['level']);
-        } else {
-            $threshold = LogLevel::EMERGENCY;
-        }
-
-        if (isset($_GET['pi'])) {
-            $pi = preg_replace('/\W/', '', $_GET['pi']);
-            $dir .= '/' . $pi;
-        }
-        $logger = new self($dir, $threshold);
-        $logger->setDateFormat('D d M Y H:i:s');
         self::$instance = $logger;
         return $logger;
     }
@@ -92,22 +80,20 @@ class CommonPlugin_Logger extends Katzgrau\KLogger\Logger
 
     public function log($level, $message, array $context = array())
     {
-        if ($this->logLevels[$this->threshold] < $this->logLevels[$level]) {
-            return;
-        }
-
         $trace = debug_backtrace(false);
 
         if (!empty($this->classes[$trace[1]['class']])) {
             $i = 1;
         } elseif (!empty($this->classes[$trace[2]['class']])) {
             $i = 2;
+        } elseif (!empty($this->classes[$trace[3]['class']])) {
+            $i = 3;
         } else {
             return;
         }
 
         $message = 
-            "{$trace[$i]['class']}::{$trace[$i]['function']}, line {$trace[$i - 1]['line']} "
+            "{$trace[$i]['class']}::{$trace[$i]['function']}, line {$trace[$i - 1]['line']}\n"
             . $message;
         parent::log($level, $message, $context);
     }
