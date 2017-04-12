8e50fc8906674d6ca5fc0d694201733367659348
Duncan Cameron <phplist@dcameron.me.uk>
2017-04-12 06:42:46 +0100

    Replace custom iterator class for query result

--- plugins/Common/DBResultIterator.php
+++ plugins/Common/DBResultIterator.php
@@ -8,100 +8,33 @@ namespace phpList\plugin\Common;
  * This file is a part of CommonPlugin.
  *
  * @category  phplist
- * @package   CommonPlugin
+ * 
  * @author    Duncan Cameron
  * @copyright 2011-2017 Duncan Cameron
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
  */
 
 /**
- * This class provides an iterator for the result of a db query
- * 
+ * This class wraps the result of a mysqli query in an iterator.
  */
-class DBResultIterator implements \Iterator, \Countable
+class DBResultIterator extends \IteratorIterator implements \Countable
 {
+    private $count;
+
     /**
-     * Current offset.
+     * Wraps the result in an interator.
+     *
+     * @param mysqli_result $result
      */
-    protected $row;
-    
-    /**
-     * MySQL result
-     */
-    protected $result;
-    
-    /**
-     * Number of rows in result set.
-     */
-    public $count;
-    
-    /**
-     * Constructor.
-     * Stores the query result.
-     * 
-     * @param resource $result
-     */
-    public function __construct($resource)
+    public function __construct(\mysqli_result $result)
     {
-        $this->result = $resource;
-        $this->row = 0;
-        $this->count = sql_num_rows($resource);
-    }
-    
-    public function __destruct()
-    {
-    }
-    
-    /**
-     * Returns next row of result data.
-     * 
-     * @return object $row
-     */
-    public function current()
-    {
-        return sql_fetch_assoc($this->result);
+        parent::__construct($result);
+        $this->count = $result->num_rows;
     }
         
     /**
-     * Returns current row number
-     */
-    public function key()
-    {
-        return $this->row;
-    }
-    
-    /** 
-     * Increments the row number
-     * 
-     */
-    public function next()
-    {
-        ++$this->row;
-    }
-    
-    /**
-     * Resets the row number and seeks the result back.
-     * 
-     */
-    public function rewind()
-    {
-        $this->row = 0;
-    }
-    
-    /**
-     * Returns whether current row number is a valid row based on total
-     * number of available rows in result resource.
-     * 
-     * @return bool
-     */
-    public function valid()
-    {
-        return (bool) ($this->row < $this->count);
-    }
-
-    /**
-    * Implementation of Countable interface
-    * Returns the number of rows in the result
+     * Implementation of Countable interface.
+     * Returns the number of rows in the result.
      * 
      * @return integer
      */
