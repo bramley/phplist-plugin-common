5eb2a3481c84ad10c21060f0e6178f4f824340ad
Duncan Cameron <phplist@dcameron.me.uk>
2017-12-17 17:45:37 +0000

    Added class to send file to browser

--- plugins/CommonPlugin/image.php
+++ plugins/CommonPlugin/image.php
@@ -11,51 +11,13 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
  */
 
+namespace phpList\plugin\Common;
+
 /**
- *  This page serves plugin images
- *
+ *  This page serves plugin images.
  */
-
-function CommonPlugin_ServeImage($image)
-{
-    $defaultExpire = 604800;
-    $filepath = dirname(__FILE__) . '/images/' . basename($image);
-
-    if (!file_exists($filepath)) {
-        header('HTTP/1.0 404 Not Found');
-        return;
-    }
-    $mtime = new DateTime('@' . filemtime($filepath));
-    $expiry = new DateTime();
-    $expiry->add(new DateInterval('PT' . $defaultExpire . 'S'));
-    header('Expires: ' . $expiry->format(DateTime::RFC1123));
-    header("Cache-Control: max-age=$defaultExpire");
-    header('Last-Modified: ' . $mtime->format(DateTime::RFC1123));
-    header('Pragma:');
-
-    if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-        $since = new DateTime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
-
-        if ($mtime <= $since) {
-            header('HTTP/1.1 304 Not Modified');
-            return;
-        }
-    }
-
-    $ext = pathinfo($filepath, PATHINFO_EXTENSION);
-    $cTypes = array(
-        'png' => 'png',
-        'gif' => 'gif',
-        'jpg' => 'jpeg',
-        'jpeg' => 'jpeg',
-        'bmp' => 'bmp'
-    );
-    $type = isset($cTypes[$ext]) ? $cTypes[$ext] : 'jpeg';
-    header("Content-type: image/$type");
-    header('Content-Length: ' . filesize($filepath));
-    readfile($filepath);
-}
-
+$server = new FileServer();
 ob_end_clean();
-CommonPlugin_ServeImage($_GET['image']);
+$server->serveFile($plugins['CommonPlugin']->coderoot . 'images/' . basename($_GET['image']));
+
 exit;
