205c45d2c748b6641dca7f3d74484b95a16336aa
Duncan Cameron <phplist@dcameron.me.uk>
2022-08-13 09:10:48 +0100

    Copy the core phplist functions for shortening text from https://github.com/phpList/phplist3/pull/877

--- plugins/CommonPlugin/functions.php
+++ plugins/CommonPlugin/functions.php
@@ -37,3 +37,64 @@ function splitIntoLines($value)
 
     return $lines;
 }
+
+/*
+ * shortenText
+ *
+ * Shorten text for use by shortenTextDisplay() but also stand-alone.
+ *
+ * Define multibyte-string aware/unaware function depending on whether the mbstring extension is available
+ * see https://github.com/phpList/phplist3/pull/10
+ */
+if (!function_exists('mb_strlen')) {
+    // mbstring unavailable
+    function shortenText($text, $max = 30)
+    {
+        if (strlen($text) > $max) {
+            if ($max < 30) {
+                $shortened = substr($text, 0, $max - 4).' ... ';
+            } else {
+                $shortened = substr($text, 0, 20).' ... '.substr($text, -10);
+            }
+        } else {
+            $shortened = $text;
+        }
+
+        return $shortened;
+    }
+} else {
+    // mbstring available
+    function shortenText($text, $max = 30)
+    {
+        if (mb_strlen($text) > $max) {
+            if ($max < 30) {
+                $shortened = mb_substr($text, 0, $max - 4).' ... ';
+            } else {
+                $shortened = mb_substr($text, 0, 20).' ... '.mb_substr($text, -10);
+            }
+        } else {
+            $shortened = $text;
+        }
+
+        return $shortened;
+    }
+}
+
+/*
+ * shortenTextDisplay
+ *
+ * mostly used for columns in listings to retrict the width, particularly on mobile devices
+ * it will show the full text as the title tip but restrict the size of the output
+ *
+ * will also place a space after / and @ to facilitate wrapping in the browser
+ *
+ */
+function shortenTextDisplay($text, $max = 30)
+{
+    $display = preg_replace('!^https?://!i', '', $text);
+    $display = shortenText($display, $max);
+    $display = str_replace('/', '/&#x200b;', $display);
+    $display = str_replace('@', '@&#x200b;', $display);
+
+    return sprintf('<span title="%s">%s</span>', htmlspecialchars($text), $display);
+}
