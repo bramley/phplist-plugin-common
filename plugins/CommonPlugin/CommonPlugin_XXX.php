ca3f8033a053edf67fd033a77e78a1ebe772dcd0
Duncan Cameron <3147688+bramley@users.noreply.github.com>
2023-12-12 23:02:06 +0000

    Replace files for legacy classes by a single file

---
+++ plugins/CommonPlugin/CommonPlugin_XXX.php
@@ -0,0 +1,105 @@
+<?php
+
+abstract class CommonPlugin_BaseController extends phpList\plugin\Common\BaseController
+{
+}
+class CommonPlugin_Config extends phpList\plugin\Common\Config
+{
+}
+abstract class CommonPlugin_ControllerFactoryBase extends phpList\plugin\Common\ControllerFactoryBase
+{
+}
+class CommonPlugin_ControllerFactory extends phpList\plugin\Common\ControllerFactory
+{
+}
+abstract class CommonPlugin_Controller extends phpList\plugin\Common\Controller
+{
+}
+class CommonPlugin_DAO extends phpList\plugin\Common\DAO
+{
+}
+class CommonPlugin_DAO_Attribute extends phpList\plugin\Common\DAO\Attribute
+{
+}
+class CommonPlugin_DAO_Config extends phpList\plugin\Common\DAO\Config
+{
+}
+class CommonPlugin_DAO_List extends phpList\plugin\Common\DAO\Lists
+{
+}
+class CommonPlugin_DAO_Message extends phpList\plugin\Common\DAO\Message
+{
+}
+class CommonPlugin_DAO_User extends phpList\plugin\Common\DAO\User
+{
+}
+class CommonPlugin_DB extends phpList\plugin\Common\DB
+{
+}
+class CommonPlugin_DBResultIterator extends phpList\plugin\Common\DBResultIterator
+{
+}
+
+abstract class CommonPlugin_Exception extends phpList\plugin\Common\Exception
+{
+}
+class CommonPlugin_ExportCSV extends phpList\plugin\Common\ExportCSV
+{
+}
+class CommonPlugin_ExportXML extends phpList\plugin\Common\ExportXML
+{
+}
+class CommonPlugin_HelpManager extends phpList\plugin\Common\HelpManager
+{
+}
+class CommonPlugin_HtmlToPdf extends phpList\plugin\Common\HtmlToPdf
+{
+}
+class CommonPlugin_I18N extends phpList\plugin\Common\I18N
+{
+}
+interface CommonPlugin_IExportable extends phpList\plugin\Common\IExportable
+{
+}
+class CommonPlugin_ImageTag extends phpList\plugin\Common\ImageTag
+{
+}
+interface CommonPlugin_IPopulator extends phpList\plugin\Common\IPopulator
+{
+}
+class CommonPlugin_Listing extends phpList\plugin\Common\Listing
+{
+}
+class CommonPlugin_Logger extends phpList\plugin\Common\Logger
+{
+}
+class CommonPlugin_Main extends phpList\plugin\Common\Main
+{
+}
+abstract class CommonPlugin_Model extends phpList\plugin\Common\Model
+{
+}
+class CommonPlugin_PageLink extends phpList\plugin\Common\PageLink
+{
+}
+class CommonPlugin_Pager extends phpList\plugin\Common\Pager
+{
+}
+class CommonPlugin_PageURL extends phpList\plugin\Common\PageURL
+{
+}
+class CommonPlugin_Populator extends phpList\plugin\Common\Populator
+{
+}
+class CommonPlugin_Tabs extends phpList\plugin\Common\Tabs
+{
+}
+class CommonPlugin_Toolbar extends phpList\plugin\Common\Toolbar
+{
+}
+class CommonPlugin_WebblerListing extends phpList\plugin\Common\WebblerListing
+{
+}
+class CommonPlugin_Widget extends phpList\plugin\Common\Widget
+{
+}
